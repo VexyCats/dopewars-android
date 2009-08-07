4d98d22343db1ca7cc5e95b4b4f24951e631f767
joe.daverin <joe.daverin@7885ad3a-6f7f-11de-9a45-5127b2b2b1b8>
2009-08-07 07:47:42 +0000

    Still gross and buggy but there is a rudimentary buying dialog sequence and I finally realized what was messing with the no title feature on the dialogs (must be set BEFORE setContentView)

--- src/com/daverin/dopewars/Global.java
+++ src/com/daverin/dopewars/Global.java
@@ -5,25 +5,6 @@ import java.util.Vector;
 
 public class Global {
 	
-	public static class Drug {
-		public String drug_name_;
-		public int base_price_;
-		public int range_;
-		public boolean outlier_high_;
-		public double outlier_high_probability_;
-		public double outlier_high_multiplier_;
-		public boolean outlier_low_;
-		public double outlier_low_probability_;
-		public double outlier_low_multiplier_;
-	}
-	
-//	public static long current_dealer_id_;
-	
 	// Random number generator!
     public static Random rand_gen_ = new Random();
-    
-    // Game setup information, set by the main menu and used by the game.
-    //public static int base_drug_count_;
-	//public static int drug_count_variance_;
-	//public static Vector<Drug> available_drugs_;
 }
