ea531282ffd74963bae746393cf01a3dab75383f
joe.daverin <joe.daverin@7885ad3a-6f7f-11de-9a45-5127b2b2b1b8>
2009-09-27 22:57:21 +0000

    update comments in the data adapter class

--- trunk/src/com/daverin/dopewars/DealerDataAdapter.java
+++ trunk/src/com/daverin/dopewars/DealerDataAdapter.java
@@ -1,3 +1,9 @@
+/**
+ * This file isolates the rest of the game from database access concerns. There are two basic
+ * tables, one for game configuration (how a game should be run) and the other for current
+ * dealer information (includes the current game state).
+ */
+
 package com.daverin.dopewars;
 
 import android.content.ContentValues;
@@ -46,13 +52,8 @@ public class DealerDataAdapter {
 	
 	private boolean dealer_table_checked_;
 	private boolean game_table_checked_;
-	
-	/**
-	 * ========================================================================
-	 * Database-level
-	 * ========================================================================
-	 */
-	
+
+	// When a database adapter is first created none of the tables have been checked.
 	public DealerDataAdapter(Context ctx) {
 		this.context = ctx;
 		dealerDBHelper = new DealerDatabaseHelper(context);
@@ -60,6 +61,8 @@ public class DealerDataAdapter {
 		game_table_checked_ = false;
 	}
 	
+	// This inner class primarily creates tables if they don't exist and handles version
+	// number checked when the database schema is changed.
 	private static class DealerDatabaseHelper extends SQLiteOpenHelper {
 		DealerDatabaseHelper(Context context) {
 			super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -90,6 +93,8 @@ public class DealerDataAdapter {
 		dealerDBHelper.close();
 	}
 	
+	// Make sure that the required dealer info is saved. The dealer table should only ever have
+	// one active row.
 	public void initDealerInfo() {
 		if (dealer_table_checked_) return;
 		
@@ -111,6 +116,7 @@ public class DealerDataAdapter {
 		db.insert(DEALER_INFO_TABLE, null, initial_dealer_info);
 	}
 	
+	// Retrieve a named column from the first row of the dealer info.
 	public String getDealerString(String key) {
 		initDealerInfo();
 		Cursor cursor = db.query(true, DEALER_INFO_TABLE,
@@ -123,17 +129,18 @@ public class DealerDataAdapter {
 		}
 		return "";
 	}
+	
+	// Set a named column in the first row of the dealer info.
 	public void setDealerString(String key, String new_value) {
 		initDealerInfo();
 		ContentValues args = new ContentValues();
 		args.put(key, new_value);
 		db.update(DEALER_INFO_TABLE, args, null, null);
 	}
-	
-	/**
-	 * Game strings table
-	 */
-	
+
+	// Similar to with the dealer info table, this checks that the required game info is saved.
+	// The game table always has four active rows (one corresponding to each button on the
+	// main menu).
 	public void initGameInfo() {
 		if (game_table_checked_) return;
 		
@@ -193,6 +200,7 @@ public class DealerDataAdapter {
 		db.insert(GAME_STRINGS_TABLE, null, initial_game_info_4);
 	}
 	
+	// Get the game settings string for the specified game, or main menu button, number.
 	public String getGameString(int game) {
 		initGameInfo();
 		Cursor cursor = db.query(true, GAME_STRINGS_TABLE,
@@ -207,6 +215,8 @@ public class DealerDataAdapter {
 		}
 		return "";
 	}
+	
+	// Set the game settings string for the specified game, or main menu button, number.
 	public void setGameString(int game, String game_strings) {
 		initGameInfo();
 		ContentValues args = new ContentValues();
